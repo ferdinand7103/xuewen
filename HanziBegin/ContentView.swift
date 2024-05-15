bfde6cd0b223ad839905e71f742f9c177323aeb4
ferdinand7103 <ferdinand7103@gmail.com>
2024-05-15 15:26:40 +0700

    Initial Commit

---
+++ HanziBegin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HanziBegin
+//
+//  Created by Ferdinand Jacques on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HanziBegin/HanziBeginApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanziBeginApp.swift
+//  HanziBegin
+//
+//  Created by Ferdinand Jacques on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HanziBeginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
