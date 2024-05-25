3ac2ba207e8729a9bfad276ecc216ab590e368b8
ferdinand7103 <ferdinand7103@gmail.com>
2024-05-25 11:13:35 +0700

    View update

---
+++ HanziBegin/Display.swift
@@ -0,0 +1,8 @@
+//
+//  Display.swift
+//  HanziBegin
+//
+//  Created by Ferdinand Jacques on 24/05/24.
+//
+
+import Foundation
---
+++ HanziBegin/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  HanziBegin
+//
+//  Created by Ferdinand Jacques on 24/05/24.
+//
+
+import Foundation
